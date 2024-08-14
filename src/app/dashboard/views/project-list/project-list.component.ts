a6208cbd5fbc65171cb9caea8152672eb33b8858
jps27CSE <jackpritomsoren@hotmail.com>
2024-08-14 12:10:17 +0600

    f

--- src/app/dashboard/views/project-list/project-list.component.ts
+++ src/app/dashboard/views/project-list/project-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { NgIf } from '@angular/common';
+import { NgForOf, NgIf } from '@angular/common';
 import { DateFormatPipe } from '../dashboard/date-format.pipe';
 import { AuthService } from '../../../core/services/auth/auth.service';
 import { Router } from '@angular/router';
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-project-list',
   standalone: true,
-  imports: [NgIf, DateFormatPipe],
+  imports: [DateFormatPipe, NgIf, NgForOf],
   templateUrl: './project-list.component.html',
   styleUrls: ['./project-list.component.css'],
 })
